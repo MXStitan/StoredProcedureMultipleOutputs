14315b8010b24542e5d9178a693232a99e808503
MXStitan <97943946+MXStitan@users.noreply.github.com>
2022-07-08 12:39:15 +0530

    Add files via upload

---
+++ StoredProcedureList/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.entites.Response;
+//import com.example.demo.entites.Studentrollno;
+
+public interface StudentService {
+
+	 public Response getCustomerDetail(int rollno);
+
+}
